3acc149612d8b35586608e1daede61ff87bde339
Ollie Riches <ollie.riches@btinternet.com>
2012-12-31 17:38:19 +0000

    Updated version number for build

--- Simple.Rest/Properties/AssemblyInfo.cs
+++ Simple.Rest/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Simple.Rest Copyright © 2012")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
